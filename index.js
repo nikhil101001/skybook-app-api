d2befad3f15ea048177e62e36d25032bfa554a70
NIKHIL SHAMRA <nikhilsharma101001@gmial.com>
2023-12-29 21:02:05 +0530

    added get user by email

--- index.js
+++ index.js
@@ -200,6 +200,23 @@ app.get("/get-users", async (req, res) => {
   }
 });
 
+// get user by email
+app.get("/get-user/:email", async (req, res) => {
+  const { email } = req.params;
+  console.log(email);
+  try {
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(200).json(user);
+    } else {
+      return res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Error getting user" });
+  }
+});
+
 // Add a new user
 app.post("/add-user", async (req, res) => {
   const { name, email } = await req.body;
